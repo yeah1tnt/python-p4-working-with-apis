9fd4150653349ccd32a4389bb7ea5b4449bd2621
Tong Tang <pichip22@hotmail.com>
2023-09-12 18:51:25 -0400

    completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -10,6 +10,18 @@ class GetPrograms:
     response = requests.get(URL)
     return response.content
 
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
